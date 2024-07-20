715ccb8a9471fb0dd1603c5955d2d4a2965583e4
Abdullah Kamal <abdallah10kamal@gmail.com>
2024-07-20 03:25:00 +0300

    Add Main Controllers.

---
+++ Sakany.API/Controllers/Errors/ErrorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sakany.Shared.Responses;
+
+namespace ECommerceAPI.API.Controllers.Errors
+{
+    [Route("Api/V{version:apiVersion}/[controller]/{code}")]
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorsController : ControllerBase
+    {
+        public IActionResult Error(int code)
+        {
+            var responseHandler = new ResponseHandler();
+
+            return code switch
+            {
+                StatusCodes.Status401Unauthorized => Unauthorized(responseHandler.Unauthorized<object>("Unauthorized or Invalid token.")),
+                StatusCodes.Status403Forbidden => StatusCode(StatusCodes.Status403Forbidden, responseHandler.Forbidden<object>("Access Forbidden.")),
+                StatusCodes.Status404NotFound => NotFound(responseHandler.NotFound<object>("Resource Not Found.")),
+                StatusCodes.Status405MethodNotAllowed => StatusCode(code, responseHandler.MethodNotAllowed<object>()),
+                _ => StatusCode(code, responseHandler.ServerError<object>("An unexpected error occurred."))
+            };
+        }
+    }
+}
